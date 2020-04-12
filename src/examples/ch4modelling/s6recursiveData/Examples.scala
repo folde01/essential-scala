ee9d2d8e0313d8ecb00f615ba95ea476719d098d
Jesse Oldershaw <jesse.oldershaw@rpharms.com>
2020-04-12 13:19:22 +0100

    4.6 recursive data - examples

---
+++ src/examples/ch4modelling/s6recursiveData/Examples.scala
@@ -0,0 +1,22 @@
+package examples.ch4modelling.s6recursiveData
+
+sealed trait IntList
+case object End extends IntList
+final case class Pair(head: Int, tail: IntList) extends IntList
+
+object foo extends App {
+  val d = End
+  val c = Pair(3, d)
+  val b = Pair(2, c)
+  val a = Pair(1, b)
+
+  val example: Pair = Pair(1, Pair(2, Pair(3, End) ))
+  assert(sum(example) == 6)
+  assert(sum(example.tail) == 5)
+  assert(sum(End) == 0)
+
+  def sum(list: IntList): Int = list match {
+    case End => 0
+    case Pair(head, tail) => head + sum(tail)
+  }
+}
