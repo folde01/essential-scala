5758d42768567f639e5a4265086172f38d15c675
Jesse Oldershaw <jesse.oldershaw@rpharms.com>
2020-04-13 11:49:22 +0100

    part 2 of ex 4.6.3.2 forest of trees

--- src/exercises/ch4modelling/s6recursiveData/Ex4_6_3_2_forestOfTrees.scala
+++ src/exercises/ch4modelling/s6recursiveData/Ex4_6_3_2_forestOfTrees.scala
@@ -1,6 +1,38 @@
 package exercises.ch4modelling.s6recursiveData
 
-sealed trait Tree
-final case class Node(left: Tree, right: Tree) extends Tree
-final case class Leaf(element: Int) extends Tree
+object patmatTree extends App {
+  sealed trait Tree {
+    def sum: Int = this match {
+      case Leaf(el) => el
+      case Node(l, r) => l.sum + r.sum
+    }
+  }
+  final case class Node(left: Tree, right: Tree) extends Tree
+  final case class Leaf(element: Int) extends Tree
+
+  assert(Leaf(1).sum == 1)
+  assert(Node(Leaf(1), Leaf(2)).sum == 3)
+  assert(Node(Leaf(1), Node(Leaf(1), Leaf(2))).sum == 4)
+  assert(Node(Node(Leaf(1), Node(Leaf(1), Leaf(2))),  Node(Leaf(1), Leaf(2))).sum == 7)
+
+}
+
+
+
+object polymorphismTree extends App {
+  sealed trait Tree {
+    def sum: Int
+  }
+  final case class Node(left: Tree, right: Tree) extends Tree {
+    def sum: Int = left.sum + right.sum
+  }
+  final case class Leaf(element: Int) extends Tree {
+    def sum: Int = element
+  }
+
+  assert(Leaf(1).sum == 1)
+  assert(Node(Leaf(1), Leaf(2)).sum == 3)
+  assert(Node(Leaf(1), Node(Leaf(1), Leaf(2))).sum == 4)
+  assert(Node(Node(Leaf(1), Node(Leaf(1), Leaf(2))),  Node(Leaf(1), Leaf(2))).sum == 7)
+}
 
