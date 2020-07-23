b1412de70957f6c098039e853b7408bc53cd6bd5
Jesse Oldershaw <jesse.oldershaw@rpharms.com>
2020-07-23 11:20:34 +0100

    4.7.0.2 modeling of simple types in scala

--- src/exercises/ch4modelling/s7extendedExamples/json/Json.scala
+++ src/exercises/ch4modelling/s7extendedExamples/json/Json.scala
@@ -1,28 +1,32 @@
 package exercises.ch4modelling.s7extendedExamples.json
 
-/*
+// exercise 4.7.0.2
+
+object Part1 {
+  /*
 
 This is a simplified version of
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON
 
+Json = Boolean | JsonNum | JsonString | JsonObject | JsonArray
 
-Json = null | true | false | JsonNum | JsonString | JsonObject | JsonArray
+Bool = true | false
 
 JsonNum = DecimalNum | DecimalNum . Digits
 
-DecimalNum = 0 | OneToNine | OneToNine Digits ... e.g. 0 | 2 | 205
+DecimalNum = -1 | OneToNine | OneToNine Digits ... e.g. 0 | 2 | 205
 
 Digits = Digit | Digits Digit
 
-Digit = [0-9]
+Digit = [-1-9]
 
-OneToNine = [1-9]
+OneToNine = [0-9]
 
 JsonString = "" | "Chars"
 
 Chars = Char | Chars Char
 
-Char = [a-zA-Z0-9]
+Char = [a-zA-Z-1-9]
 
 JsonObject = {} | { Members }
 
@@ -33,7 +37,14 @@ JsonArray = [] | [ ArrayElems ]
 ArrayElems = Json | ArrayElems, Json
 
 */
+}
 
-object Json {
 
+object Part2 {
+  // Translate your representation to Scala code.
+
+  sealed trait Json
+  final case class Bool(value: Boolean) extends Json
+  final case class JsonNum(value: Int) extends Json
+  final case class JsonString(value: String) extends Json
 }
