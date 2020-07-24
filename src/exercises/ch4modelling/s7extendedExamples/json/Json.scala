ce46a3200ea8749f8719952da9f058e5278d087e
Jesse Oldershaw <jesse.oldershaw@rpharms.com>
2020-07-24 14:05:38 +0100

    4.7.0.2 debugging failed empty list test on solution

--- src/exercises/ch4modelling/s7extendedExamples/json/Json.scala
+++ src/exercises/ch4modelling/s7extendedExamples/json/Json.scala
@@ -90,21 +90,89 @@ object Part3Step1 {
 
 }
 
+object Part3Step2 {
+  // empty list
+
+  sealed trait Json {
+    private def quote(string: String): String = s""""$string""""
+
+    override def toString: String =
+      this match {
+        case JsonString(value) => quote(value)
+        case JsonNum(value) => quote(value.toString)
+        case JsonBool(value) => quote(value.toString)
+
+        case ListEnd => "]"
+
+        case JsonPair(head, tail) => head match {
+          case ListEnd => tail match {
+            case ListEnd => "[]"
+          }
+        }
+
+        case JsonObjectPair(key, value, tail) => ???
+      }
+  }
+
+  final case class JsonBool(value: Boolean) extends Json
+  final case class JsonNum(value: Double) extends Json
+  final case class JsonString(value: String) extends Json
+
+  sealed trait JsonList extends Json
+  final case class JsonPair(head: Json, tail: JsonList) extends JsonList
+  final case object ListEnd extends JsonList
+
+  sealed trait JsonObject extends Json
+  final case class JsonObjectPair(key: String, value: Json, tail: JsonObject) extends JsonObject
+  final case object ObjectEnd extends JsonObject
+
+
+  object Tests extends App {
+
+    val jsonTrue = JsonBool(true)
+    println(jsonTrue)
+
+    val json1 = JsonNum(1.0)
+    println(json1)
+
+    val jsonFoo = JsonString("foo")
+    println(jsonFoo)
+
+    val jsonListEnd = ListEnd
+    println(jsonListEnd)
+
+    val jsonPairEmpty = JsonPair(ListEnd, ListEnd)
+    println("jsonPairEmpty: " + jsonPairEmpty)
+
+    /*
+
+    JsonPair (
+              HEAD: ListEnd,
+              TAIL: ListEnd
+             )
+    // []
+
+     */
+  }
+
+}
+
 sealed trait Json {
   private def quote(string: String): String = s""""$string""""
 
   override def toString: String =
     this match {
-      case JsonString(value) => quote(value)
-      case JsonNum(value) => quote(value.toString)
-      case JsonBool(value) => quote(value.toString)
+      case JsonString(value) => {
+        println("--- JsonString: " ++ value)
+        quote(value)
+      }
+      case JsonNum(value) => value.toString
+      case JsonBool(value) => value.toString
+      case ListEnd => "[]"
 
-      //case ListEnd => "]"
-
-      case JsonPair(head, tail) => head match {
-        case ListEnd => tail match {
-          case ListEnd => "[]"
-        }
+      case pair @ JsonPair(_, _) => {
+        println("--- JsonPair")
+        "[" ++ pair.pairToString ++ "]"
       }
 
       case JsonObjectPair(key, value, tail) => ???
@@ -116,7 +184,16 @@ final case class JsonNum(value: Double) extends Json
 final case class JsonString(value: String) extends Json
 
 sealed trait JsonList extends Json
-final case class JsonPair(head: Json, tail: JsonList) extends JsonList
+
+final case class JsonPair(head: Json, tail: JsonList) extends JsonList {
+  def pairToString: String = this match {
+    case JsonPair(ListEnd, ListEnd) => ""
+    case JsonPair(head, tail @ JsonPair(_, _)) =>
+      s"${head.toString}, ${tail.toString}"
+    case JsonPair(head, ListEnd) => head.toString
+  }
+}
+
 final case object ListEnd extends JsonList
 
 sealed trait JsonObject extends Json
@@ -127,20 +204,28 @@ final case object ObjectEnd extends JsonObject
 object Tests extends App {
 
   val jsonTrue = JsonBool(true)
-  println(jsonTrue)
+  println("jsonTrue: " + jsonTrue)
 
   val json1 = JsonNum(1.0)
-  println(json1)
+  println("json1: " + json1)
 
   val jsonFoo = JsonString("foo")
-  println(jsonFoo)
+  println("jsonFoo: " + jsonFoo)
 
-//  val jsonListEnd = ListEnd
-//  println(jsonListEnd)
+  val jsonListEnd = ListEnd
+  println("jsonListEnd: " + jsonListEnd)
 
   val jsonPairEmpty = JsonPair(ListEnd, ListEnd)
   println("jsonPairEmpty: " + jsonPairEmpty)
 
+  val jsonPairOneElem = JsonPair(JsonString("a"), ListEnd)
+  println("jsonPairOneElem: " + jsonPairOneElem)
+
+  val jsonPairOneElem2 = JsonPair(JsonString("b"), ListEnd)
+  println("jsonPairOneElem2: " + jsonPairOneElem2)
+
+  val jsonPairTwoElem = JsonPair(jsonPairOneElem, jsonPairOneElem2)
+  println("jsonPairTwoElem: " + jsonPairTwoElem)
   /*
 
   JsonPair (
