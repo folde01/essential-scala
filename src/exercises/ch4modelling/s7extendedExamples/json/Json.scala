fa4a9ae05654cef65d85cee2eafb04e2d1eff9da
Jesse Oldershaw <jesse.oldershaw@rpharms.com>
2020-07-24 08:46:59 +0100

    4.7.0.2 skeleton toString and tests

--- src/exercises/ch4modelling/s7extendedExamples/json/Json.scala
+++ src/exercises/ch4modelling/s7extendedExamples/json/Json.scala
@@ -40,3 +40,51 @@ object Part2 {
   final case class JsonObjectPair(key: String, value: Json, tail: JsonObject) extends JsonObject
   final case object ObjectEnd extends JsonObject
 }
+
+  // Json.toString
+
+sealed trait Json {
+  override def toString: String = "foo"
+}
+
+final case class JsonBool(value: Boolean) extends Json
+
+final case class JsonNum(value: Double) extends Json
+
+final case class JsonString(value: String) extends Json
+
+sealed trait JsonList extends Json
+
+final case class JsonPair(head: Json, tail: JsonList) extends JsonList
+
+final case object ListEnd extends JsonList
+
+sealed trait JsonObject extends Json
+
+final case class JsonObjectPair(key: String, value: Json, tail: JsonObject) extends JsonObject
+
+final case object ObjectEnd extends JsonObject
+
+object Tests {
+
+  JsonPair (JsonString ("a string"), JsonPair (JsonNum (1.0), JsonPair (JsonBool
+  (true), ListEnd) ) ).toString
+  // res0: String = ["a string", 1.0, true]
+  JsonObjectPair (
+    "a", JsonPair (JsonNum (1.0), JsonPair (JsonNum (2.0), JsonPair (JsonNum
+    (3.0), ListEnd) ) ),
+    JsonObjectPair (
+      "b", JsonPair (JsonString ("a"), JsonPair (JsonString ("b"), JsonPair (
+        JsonString ("c"), ListEnd) ) ),
+      JsonObjectPair (
+        "c", JsonObjectPair ("doh", JsonBool (true),
+          JsonObjectPair ("ray", JsonBool (false),
+            JsonObjectPair ("me", JsonNum (1.0), ObjectEnd) ) ),
+        ObjectEnd
+      )
+    )
+  ).toString
+  
+}
+
+// res1: String = {"a": [1.0, 2.0, 3.0], "b":
