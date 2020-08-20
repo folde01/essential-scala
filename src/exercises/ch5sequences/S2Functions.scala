fde440f7a1ca1b4fe727f23944e3ee773b86cb46
Jesse Oldershaw <jesse.oldershaw@rpharms.com>
2020-08-20 11:42:39 +0100

    5.2.3.1 part 2

--- src/exercises/ch5sequences/S2Functions.scala
+++ src/exercises/ch5sequences/S2Functions.scala
@@ -4,11 +4,7 @@ package exercises.ch5sequences
 object Examples extends App {
 
   sealed trait IntList {
-    def length: Int =
-      this match {
-        case End => 0
-        case Pair(hd, tl) => 1 + tl.length
-      }
+
 
     def double: IntList =
       this match {
@@ -16,31 +12,85 @@ object Examples extends App {
         case Pair(hd, tl) => Pair(hd * 2, tl.double)
       }
 
+
+    def sum: Int = this match {
+      case End => 0
+      case Pair(hd, tl) => hd + tl.sum
+    }
+
+    def sumFold: Int = fold(0, (x: Int, y: Int) => x + y)
+
+    def fold(end: Int, f: (Int, Int) => Int): Int = this match {
+      case End => end
+      case Pair(hd, tl) => f(hd, tl.fold(end, f))
+    }
+
+    def productFold: Int = fold(1, (hd, tailProduct) => hd * tailProduct)
+
     def product: Int =
       this match {
         case End => 1
         case Pair(hd, tl) => hd * tl.product
       }
 
-    def sum: Int =
+    def lengthFold: Int = fold(0, (_, tailLength) => 1 + tailLength)
+
+    def length: Int =
       this match {
         case End => 0
-        case Pair(hd, tl) => hd + tl.sum
+        case Pair(_, tl) => 1 + tl.length
       }
-
-    def fold(end: Int, f: (Int, Int) => Int): Int = this match {
-      case End => end
-      case Pair(hd, tl) => f(hd, tl.fold(end, f))
-    }
   }
 
   case object End extends IntList
 
   case class Pair(hd: Int, tl: IntList) extends IntList
 
+  val two: Pair = Pair(2, End)
+  val oneTwo: Pair = Pair(1, two)
+  val oneTwoThree: Pair = Pair(1, Pair(2, Pair(3, End)))
+
+  println("assert(two.sum == 1)")
+  assert(two.sum == 2)
+  println("assert(two.sumFold == 1)")
+  assert(two.sumFold == 2)
+
+  println("assert(End.length == 0)")
+  assert(End.length == 0)
+  println("assert(two.length == 1)")
+  assert(two.length == 1)
+  println("assert(oneTwo.length == 2)")
+  assert(oneTwo.length == 2)
+
+  println("assert(End.lengthFold == 0)")
+  assert(End.lengthFold == 0)
+  println("assert(two.lengthFold == 1)")
+  assert(two.lengthFold == 1)
+  println("assert(oneTwo.lengthFold == 2)")
+  assert(oneTwo.lengthFold == 2)
+
+  println("assert(End.product == 1)")
+  assert(End.product == 1)
+  println("assert(two.product == 2)")
+  assert(two.product == 2)
+  println("assert(oneTwo.product == 2)")
+  assert(oneTwo.product == 2)
+  println("assert(oneTwoThree.product == 6)")
+  assert(oneTwoThree.product == 6)
+
+  println("assert(End.productFold == 1)")
+  assert(End.productFold == 1)
+  println("assert(two.productFold == 2)")
+  assert(two.productFold == 2)
+  println("assert(oneTwo.productFold == 2)")
+  assert(oneTwo.productFold == 2)
+  println("assert(oneTwoThree.productFold == 6)")
+  assert(oneTwoThree.productFold == 6)
+
   object add1 {
     def apply(n: Int): Int = n + 1
   }
+
   println(s"add1(2): ${add1(2)}")
 
   val hi = () => "hi!"
