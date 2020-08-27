b123d5c582689018cd63275214ced53fdee9ca7a
Jesse Oldershaw <jesse.oldershaw@rpharms.com>
2020-08-27 17:38:36 +0100

    5.3.4.1 mm's way

--- src/exercises/ch5sequences/S3GenericFolds.scala
+++ src/exercises/ch5sequences/S3GenericFolds.scala
@@ -30,16 +30,21 @@ object S3GenericFolds extends App {
   //
 
   sealed trait Tree[A] {
-    def fold[B](leaf: A => B, node: (B, B) => B): B
+//    def fold[B](leaf: A => B, node: (B, B) => B): B
+
+    def fold[B](leaf: A => B)(node: (B,B) => B): B = this match {
+        case Leaf(v: A) => leaf(v)
+        case Node(l, r) => node(l.fold(leaf)(node), r.fold(leaf)(node))
+      }
   }
 
   final case class Node[A](left: Tree[A], right: Tree[A]) extends Tree[A] {
-    def fold[B](leaf: A => B, node: (B, B) => B): B =
-      node(left.fold(leaf, node), right.fold(leaf, node))
+//    def fold[B](leaf: A => B, node: (B, B) => B): B =
+//      node(left.fold(leaf, node), right.fold(leaf, node))
   }
 
   final case class Leaf[A](elem: A) extends Tree[A] {
-    def fold[B](leaf: A => B, node: (B, B) => B): B = leaf(elem)
+//    def fold[B](leaf: A => B, node: (B, B) => B): B = leaf(elem)
   }
 
   val tree: Tree[String] =
@@ -47,23 +52,34 @@ object S3GenericFolds extends App {
       Node(Node(Leaf("is"), Leaf("human,")),
         Node(Leaf("to"), Node(Leaf("recurse"), Leaf("divine")))))
 
-  val tree2: Tree[String] = Leaf[String]("a")
 
-  println(s"tree2: ${tree2.fold[String]((str) => str, (a, b) => a)}")
+  def leaf: String => String = v => v
+  def node: (String, String) => String = (l, r) => l + " " + r
 
-  val tree3: Tree[String] = Node(tree2, tree2)
+  println(s"tree.............. ${tree.fold(leaf)(node)}")
 
-  println(s"tree3: ${tree3.fold[String]((s) => s, (a, b) => a + " " + b)}")
+//  def leaf2: String => Int = v => v.length
+//  def node2: (String, String) => Int = (l, r) => l.length + r.length
+//
+//  println(s"tree.............. ${tree.fold(leaf2)(node2)}")
 
-  println(s"tree: ${tree.fold[String]((s) => s, (a, b) => a + " " + b)}")
-
-  val tree4: Tree[Int] = Leaf(1)
-
-  println(s"tree4: ${tree4.fold[Int]((n) => n, (a, b) => a + b)}")
-
-  val tree5: Tree[Int] = Node(tree4, tree4)
-
-  println(s"tree5: ${tree5.fold[Int]((n) => n, (a, b) => a + b)}")
+//  val tree2: Tree[String] = Leaf[String]("a")
+//
+//  println(s"tree2: ${tree2.fold[String]((str) => str, (a, b) => a)}")
+//
+//  val tree3: Tree[String] = Node(tree2, tree2)
+//
+//  println(s"tree3: ${tree3.fold[String]((s) => s, (a, b) => a + " " + b)}")
+//
+//  println(s"tree: ${tree.fold[String]((s) => s, (a, b) => a + " " + b)}")
+//
+//  val tree4: Tree[Int] = Leaf(1)
+//
+//  println(s"tree4: ${tree4.fold[Int]((n) => n, (a, b) => a + b)}")
+//
+//  val tree5: Tree[Int] = Node(tree4, tree4)
+//
+//  println(s"tree5: ${tree5.fold[Int]((n) => n, (a, b) => a + b)}")
 
 
 
